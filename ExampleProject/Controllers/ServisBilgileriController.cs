3e4b0dc52903059957b3401d3df9c01d22cafdad
yasintest <yasin.baran3@gmail.com>
2016-03-06 17:10:08 +0200

    Projelerim
    Bitirme Proje Kodlarım

---
+++ ExampleProject/Controllers/ServisBilgileriController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExampleProject.Controllers
+{
+    public class ServisBilgileriController : Controller
+    {
+        // GET: ServisBilgileri
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ExampleProject/Models/Gmail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace ExampleProject.Models
+{
+    public static class Gmail
+    {
+        public static void SendMail(string body)
+        {
+            var fromAddress = new MailAddress("yasin.baran@bil.omu.edu.tr", "Sunucu Yönetim Paneli Geribildirim");
+            var toAddress = new MailAddress("yasin.baran@bil.omu.edu.tr");
+            const string subject = "Sunucu Yönetim Paneli Geribildirim";
+            using (var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, "sinbar19@")
+                //trololol kısmı e-posta adresinin şifresi
+            })
+            {
+                using (var message = new MailMessage(fromAddress, toAddress) { Subject = subject, Body = body })
+                {
+                    smtp.Send(message);
+                }
+            }
+        }
+    }
+}
